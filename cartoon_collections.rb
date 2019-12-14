548effd2e8d7a570e6b47ba8eab0f4e0e608e7e9
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 07:09:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  
+  calls.each {|word| if word.length < 4 puts false}
 end
 
 def find_the_cheese# code an argument here
