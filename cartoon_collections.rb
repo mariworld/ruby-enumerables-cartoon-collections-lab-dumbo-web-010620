e7fde4f960eb7cff65a9f7a130263f9920511f8e
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 07:29:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,5 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.detect {|i| i == cheese_types}
-  foods.find_index
+  foods.find_all {|i| i == cheese_types}
 end
