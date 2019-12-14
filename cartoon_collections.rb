3114ffc2cbb315edbec441aeb2c7c97f6195f888
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 07:01:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   array = []
-  planeteer_calls.each {|element| puts "#{element}!"}
+  planeteer_calls.each {|element| puts array << "#{element.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
