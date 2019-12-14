b9f4408eff5655bb0ca9518d66f694b876562bfb
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 07:59:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.uniq!.detect {|i| i and i == cheese_types}
+  foods.uniq! {|i| i and i == cheese_types}
 end
