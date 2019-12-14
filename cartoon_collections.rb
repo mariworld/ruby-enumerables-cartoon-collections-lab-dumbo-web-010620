3244eac0f5f06eb63db1ae9ac360c51ff4586e48
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 06:52:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls {|element| puts "#{element,capitalize}!"}
+  planeteer_calls {|element| puts "#{element.capitalize}!"}
 
 end
 
